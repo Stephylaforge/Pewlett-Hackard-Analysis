a25c801a3595b38310bdddf50d5fac871b7e006b
Stephanie <stephanieellen1007@gmail.com>
2022-05-10 23:17:27 -0400

    add final challenge deliveries

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,52 @@
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+	e.birth_date,
+	d.from_date,
+	d.to_date
+	SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+SELECT COUNT(ut.emp_no),
+ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
